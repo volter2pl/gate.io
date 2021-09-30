d44f9bbc93be540bcae14f1f0ef3afb438d61121
root <root@app.volter.net>
2021-10-01 00:15:11 +0200

    skycamp #5

--- routes/sc.js
+++ routes/sc.js
@@ -4,45 +4,26 @@ let axios = require('axios');
 let jwt_decode = require('jwt-decode');
 
 const URL = 'https://api.jumpkeeper.com/api/';
+const headers = {"Content-Type": "application/json", "Cache-Control": "no-cache"};
 
 const login = async (username, password) => {
-    return axios.post(
-        URL + 'jwt/token',
-        {password, username},
-        {headers: {"Content-Type": "application/json", "Cache-Control": "no-cache"}}
-    );
+    return axios.post(URL + 'jwt/token', {password, username}, headers);
 };
 
 const refresh = async (refreshToken) => {
-
-    const data = { refresh_token: refreshToken};
-    const config = {
-        headers: {
-            "Content-Type": "application/json",
-            "Cache-Control": "no-cache",
-        }
-    };
-
     try {
-        const r = await axios.post(URL + 'refresh/token', data, config);
+        const r = await axios.post(URL + 'refresh/token', {refresh_token: refreshToken}, headers);
         return r.data;
-    } catch(e) {
-        console.log('error', e);
+    } catch(error) {
+        error.response.status = 401;
+        throw error;
     }
-
-    return {accessToken: "", refreshToken: ""};
 };
 
 const calendar = async (accessToken) => {
     return axios.get(
         URL + 'calendar/events?calendars=1,2,3,4,5,6,7,8,9,10,11,12,13,14',
-        {
-            headers: {
-                "Content-Type": "application/json",
-                "Cache-Control": "no-cache",
-                Authorization: 'Bearer ' + accessToken
-            }
-        }
+        {headers: {...headers, Authorization: 'Bearer ' + accessToken}}
     );
 };
 
@@ -50,72 +31,73 @@ module.exports = () => {
 
   router.get('/', async (req, res, next) => {
 
-      let accessToken = req.cookies['accessToken'];
-      let refreshToken = req.cookies['refreshToken'];
+      let accessToken = req.cookies['accessToken'] || "";
+      let refreshToken = req.cookies['refreshToken'] || "";
 
       if (req.session && accessToken.length > 0 && refreshToken.length > 0) {
-          let jwt = jwt_decode(accessToken);
-          console.log(jwt);
+          const jwt = jwt_decode(accessToken);
 
-          if ( jwt.exp <= Math.round(Date.now()/1000) ) {
-              const cred = await refresh(refreshToken);
-              accessToken = cred.accessToken;
-              refreshToken = cred.refreshToken;
-              res.cookie('accessToken', accessToken);
-              res.cookie('refreshToken', refreshToken);
-          }
+          try {
+              if ( jwt.exp <= Math.round(Date.now()/1000) ) {
+                  const cred = await refresh(refreshToken);
+                  accessToken = cred.accessToken;
+                  refreshToken = cred.refreshToken;
+                  res.cookie('accessToken', accessToken);
+                  res.cookie('refreshToken', refreshToken);
+              }
 
-          calendar(accessToken)
-          .then(data => {
-              res.render('sc/index', {
-                  title: 'login',
-                  data: JSON.stringify(data.data)
-              });
-          }).catch(error => {
-              // if 401
-              // res.redirect('/sc/login');
-              console.log(error.data);
-              res.status(400).json(error);
-          });
+              calendar(accessToken)
+              .then(data =>
+                  res.render('sc/index', {title: 'calendar', data: JSON.stringify(data.data)})
+              );
+
+          } catch(error) {
+              if (error.response.status === 401) {
+                  req.session = null;
+                  res.render('sc/login', {title: 'login'});
+              } else {
+                  res.status(400).json({error: true});
+              }
+          };
       } else {
-          res.redirect('/sc/login');
+          console.log('no session');
+          res.render('sc/login', {title: 'login'});
       }
 
   });
 
   router.get('/login', (req, res, next) => {
-        res.render('sc/login', {
-            title: 'login'
-        });
+      res.render('sc/login', {title: 'login'});
+  });
+
+  router.get('/logout', (req, res, next) => {
+      res.clearCookie('accessToken');
+      res.clearCookie('refreshToken');
+      res.render('sc/login', {title: 'login'});
   });
 
   router.post('/login', (req, res, next) => {
-      res.clearCookie('accessToken');
-      res.clearCookie('refreshToken');
-
       login(req.body.email, req.body.pass)
       .then(jumpkeeper => {
           res.cookie('accessToken', jumpkeeper.data.accessToken);
           res.cookie('refreshToken', jumpkeeper.data.refreshToken);
           res.json({redirect: '/sc/'});
       }).catch(error => {
-          console.error(error);
-          res.status(400).json('error');
+          res.status(400);
       })
   });
 
   router.get('/refresh', async (req, res) => {
-
       try {
           const jumpkeeper = await refresh(req.cookies['refreshToken']);
-
           res.cookie('accessToken', jumpkeeper.accessToken);
           res.cookie('refreshToken', jumpkeeper.refreshToken);
 
           res.json({jumpkeeper: jumpkeeper});
       } catch(error) {
           const e = error.response.data;
-          res.status(400).json({error: [e.message, e.exception, e.file, e.line]});
+          // res.status(400).json({error: [e.message, e.exception, e.file, e.line]});
+          res.status(400);
       };
 
   });
